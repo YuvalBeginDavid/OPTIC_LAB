985bcea8d575725212fedbab3415e77bfcf37f44
YuvalBeginDavid <yuvalbegindavid@gmail.com>
2025-03-29 17:52:09 +0300

    Update LAB_MODEL.py

--- LAB_MODEL.py
+++ LAB_MODEL.py
@@ -11,9 +11,21 @@ img_BAB = Image.open("BAB.png")
 img_BBA = Image.open("BBA.png")
 img_BBB = Image.open("BBB.png")
 
-# Title and subtitle
-st.title('MMW LAB EXPERIMENT ILLUSTRATION')
-st.write('Yuval & Adi For Prof. Yosef Pinhasi')
+# Loading graph images
+graph_AAA = Image.open("GAAA.png")
+graph_AAB = Image.open("GAAB.png")
+graph_ABA = Image.open("GABA.png")
+graph_ABB = Image.open("GABB.png")
+graph_BAA = Image.open("GBAA.png")
+graph_BAB = Image.open("GBAB.png")
+graph_BBA = Image.open("GBBA.png")
+graph_BBB = Image.open("GBBB.png")
+
+# Title and subtitle with smaller font size using markdown
+st.markdown("""
+# MMW LAB EXPERIMENT ILLUSTRATION
+### Yuval & Adi For Prof. Yosef Pinhasi
+""", unsafe_allow_html=True)
 
 # Creating a horizontal layout for switches
 col1, col2, col3 = st.columns(3)
@@ -24,25 +36,26 @@ with col2:
 with col3:
     lab_light_status = st.checkbox("LAB LIGHT ON/OFF", False)
 
-# Determining the image based on the switches' status
-def get_image(filter, gdd, lab_light):
+# Determining the image and graph based on the switches' status
+def get_image_and_graph(filter, gdd, lab_light):
     if not filter and not gdd and not lab_light:
-        return img_AAA
+        return img_AAA, graph_AAA
     elif not filter and not gdd and lab_light:
-        return img_AAB
+        return img_AAB, graph_AAB
     elif not filter and gdd and not lab_light:
-        return img_ABA
+        return img_ABA, graph_ABA
     elif not filter and gdd and lab_light:
-        return img_ABB
+        return img_ABB, graph_ABB
     elif filter and not gdd and not lab_light:
-        return img_BAA
+        return img_BAA, graph_BAA
     elif filter and not gdd and lab_light:
-        return img_BAB
+        return img_BAB, graph_BAB
     elif filter and gdd and not lab_light:
-        return img_BBA
+        return img_BBA, graph_BBA
     elif filter and gdd and lab_light:
-        return img_BBB
+        return img_BBB, graph_BBB
 
-# Displaying the image
-current_image = get_image(filter_status, gdd_status, lab_light_status)
+# Displaying the image and graph
+current_image, current_graph = get_image_and_graph(filter_status, gdd_status, lab_light_status)
 st.image(current_image, use_container_width=True)
+st.image(current_graph, use_container_width=True)

82fbac4fc6f541c9f01dcf52fd740f9fd765abab
YuvalBeginDavid <yuvalbegindavid@gmail.com>
2025-03-29 18:50:11 +0300

    Update LAB_MODEL.py

--- LAB_MODEL.py
+++ LAB_MODEL.py
@@ -72,7 +72,7 @@ def get_image_and_graph(filter, gdd, lab_light):
     # Draw text on the image
     draw = ImageDraw.Draw(base_img)
     texts = ["Photoreceiver", "GDD Holder", "Lens", "Hyperbolic Mirror", "Radiation Source"]
-    x_positions = [20, 120, 220, 320, 420]  # Example positions, adjust as necessary
+    x_positions = [60, 220, 320, 420, 520]  # Example positions, adjust as necessary
     y_position = base_img.height - 250  # Adjust vertical position as necessary
     for text, x in zip(texts, x_positions):
         draw.text((x, y_position), text, font=font, fill="white")

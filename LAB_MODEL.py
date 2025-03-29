f2904d2df5023a3c050654dc2b8e685148927460
YuvalBeginDavid <yuvalbegindavid@gmail.com>
2025-03-29 17:39:01 +0300

    Update LAB_MODEL.py

--- LAB_MODEL.py
+++ LAB_MODEL.py
@@ -2,14 +2,14 @@ import streamlit as st
 from PIL import Image
 
 # Loading images
-img_AAA = Image.open("AAA.jpg")
-img_AAB = Image.open("AAB.jpg")
-img_ABA = Image.open("ABA.jpg")
-img_ABB = Image.open("ABB.jpg")
-img_BAA = Image.open("BAA.jpg")
-img_BAB = Image.open("BAB.jpg")
-img_BBA = Image.open("BBA.jpg")
-img_BBB = Image.open("BBB.jpg")
+img_AAA = Image.open("AAA.png")
+img_AAB = Image.open("AAB.png")
+img_ABA = Image.open("ABA.png")
+img_ABB = Image.open("ABB.png")
+img_BAA = Image.open("BAA.png")
+img_BAB = Image.open("BAB.png")
+img_BBA = Image.open("BBA.png")
+img_BBB = Image.open("BBB.png")
 
 # Title
 st.title('Electric Lab - Control Panel')
